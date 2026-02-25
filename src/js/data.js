46d7de0c05fa51dadb2ddb8f882323f8739dc0d0
FAYnim <yuana6898@gmail.com>
2026-02-25 16:09:20 +0700

    remove: data.js

--- src/js/data.js
+++
@@ -1,9 +0,0 @@
-fetch('https://panel.faydev.my.id/data/app-config.json')
-    .then(response => response.json())
-    .then(data => {
-        // Use the fetched data to update the UI
-        console.log(data);
-    })
-    .catch(error => {
-        console.error('Error fetching data:', error);
-    });
